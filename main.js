bc3bf83201322ef678595b7f4fde376e91ebd99c
digitalicarus <digitalicarus@gmail.com>
2014-04-27 18:14:09 -0500

    cow too aggressive, switching to cube

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$.get('data/cow.ply')
+$.get('data/cube.ply')
 .then(function (cow) {
 	var beef          = new ply(cow)
 	,   body          = document.body
@@ -17,16 +17,20 @@ $.get('data/cow.ply')
 		tmpDom.setAttribute('data-face', i);
 		tmpTransform = isoInverse.multiply($M([
 			beef.vertices[v[1]].concat(1),
-			beef.vertices[v[1]].concat(1),
-			beef.vertices[v[1]].concat(1),
+			beef.vertices[v[2]].concat(1),
+			beef.vertices[v[3]].concat(1),
 			[1,1,1,1]
 		]));
 		//tmpDom.setAttribute('data-transform', tmpTransform.elements);
-		tmpDom.style.webkitTransform = 'matrix3d(' + Array.prototype.concat.apply([],tmpTransform.elements).map(function(v) {return v*100;}).toString() + ')';
+		tmpDom.style.webkitTransform = 'matrix3d(' + Array.prototype.concat.apply([],tmpTransform.elements).map(function(v) {return v*10;}).toString() + ')';
+		tmpDom.innerHTML = 'a';
 		domFaces.push(tmpDom);
 		container.appendChild(tmpDom);
 	});
 
+	container.style.webkitTransformOrigin = "0 0 0";
+	container.style.webkitTransform = 'translate3d(40px, 40px, 40px) rotateX(38deg) rotateZ(30deg)';
+	container.style.webKitPerspective = '160px';
 	// should I be using document fragments?
 	body.appendChild(container);
 
